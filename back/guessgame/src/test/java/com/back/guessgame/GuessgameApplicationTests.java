192b12d9101db1130cebfb6c190a770af92d0a2d
Arthur Pascal <arthur.pascal@epitech.eu>
2024-06-17 16:14:17 +0200

    feat: spring initialized

---
+++ back/guessgame/src/test/java/com/back/guessgame/GuessgameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back.guessgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessgameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
