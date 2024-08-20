7cca140defc0b4c902a31dca6e7a92ae55146568
Hugo Delattre <delattre.hugo.24@gmail.com>
2024-08-20 16:13:28 +0200

    feat: add header

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -5,10 +5,9 @@ import { StompSessionProvider } from 'react-stomp-hooks';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { Toaster } from '@/components/ui/toaster';
-import Image from 'next/image';
-import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { DiamondWall } from '@/components/layout/diamond';
+import { Header } from '@/components/Header/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
     const router = useRouter();
@@ -29,15 +28,7 @@ export default function App({ Component, pageProps }: AppProps) {
         <StompSessionProvider url="http://localhost:8080/ws-endpoint">
             <QueryClientProvider client={queryClient}>
                 <HydrationBoundary state={pageProps.dehydratedState}>
-                    <Link href={'/'}>
-                        <Image
-                            src="/guezgame-logo-horizontal.png"
-                            alt="GuezGame logo"
-                            width={250}
-                            height={100}
-                            className="absolute hidden md:block left-2 top-5 z-[1000] hover:z-[1001]"
-                        />
-                    </Link>
+                    {!isGameplayRoute && <Header />}
                     <Component {...pageProps} />
                     {/* <ReactQueryDevtools initialIsOpen={false} /> */}
                     {!isGameplayRoute && <DiamondWall />}
--- front/src/pages/register/index.tsx
+++ front/src/pages/register/index.tsx
@@ -1,19 +1,9 @@
 import { RegisterForm } from '@/components/forms/RegisterForm';
-import { Button } from '@/components/ui/button';
-import { useRegister } from '@/hooks/auth';
-import Link from 'next/link';
 
 const RegisterPage = () => {
     return (
         <>
-            <div className="flex justify-end items-end h-full py-[39px] px-[47px]">
-                <Link href={'/login'}>
-                    <Button variant="default" className="bg-amber-300">
-                        Se connecter
-                    </Button>
-                </Link>
-            </div>
-            <div className="w-full flex justify-center flex-col items-center gap-11 mt-[10px] mb-[50px]">
+            <div className="w-full flex justify-center flex-col items-center gap-11 mt-[85px] mb-[50px]">
                 <h1 className="text-amber-300 text-[64px] font-Bangers">S'INSCRIRE</h1>
                 <RegisterForm />
             </div>
