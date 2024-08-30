3abc003003c8cfad5ee291e32da2d3867d969d51
AmenoUP <144673541+AmenoUP@users.noreply.github.com>
2024-08-30 15:05:49 +0200

    typage typscript dans index

--- front/src/pages/Titre/index.tsx
+++ front/src/pages/Titre/index.tsx
@@ -5,20 +5,25 @@ import TitlesDisplay from '../../components/titreGame/TitleDisplay';
 import Voting from '../../components/titreGame/VotingTitle';
 import ResultsDisplay from '../../components/titreGame/ResultsDisplay';
 
+interface Title {
+  text: string;
+  author: string;
+}
 
-const index = () => {
-  const images = [
+const Index: React.FC = () => {
+  const images: string[] = [
     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQMI5kxltdR3KFgBmjDtLtw_0T2oE9PJpvR9A&s',
     'https://i.pinimg.com/736x/76/60/8b/76608be0e55bf6f142bf2ff6bafb465b.jpg',
     'https://i.pinimg.com/736x/a9/ef/a9/a9efa9e0d9a868bf182a920938c0c094.jpg',
   ];
-  const [titles, setTitles] = useState([]);
-  const [currentImage, setCurrentImage] = useState(null);
-  const [step, setStep] = useState(1);
-  const [ownTitle, setOwnTitle] = useState('');
-  const [votes, setVotes] = useState({});
-  const randomIndex =Math.floor(Math.random() * images.length);
+
+  const [titles, setTitles] = useState<Title[]>([]);
+  const [currentImage, setCurrentImage] = useState<string | null>(null);
+  const [step, setStep] = useState<number>(1);
+  const [ownTitle, setOwnTitle] = useState<string>('');
+  const [votes, setVotes] = useState<Record<string, number>>({});
   
+  const randomIndex = Math.floor(Math.random() * images.length);
 
   useEffect(() => {
     if (currentImage === null) {
@@ -29,14 +34,15 @@ const index = () => {
   const handleNextStep = () => {
     setStep((prevStep) => prevStep + 1);
   };
-  const handleTitleSubmit = (title) => {
-    const newTitle = { text: title, author: 'Your Name' };
+
+  const handleTitleSubmit = (title: string) => {
+    const newTitle: Title = { text: title, author: 'Your Name' };
     setTitles([...titles, newTitle]);
     setOwnTitle(title);
     setStep(3);
   };
 
-  const handleVote = (title) => {
+  const handleVote = (title: string) => {
     setVotes((prevVotes) => ({
       ...prevVotes,
       [title]: (prevVotes[title] || 0) + 1,
@@ -46,20 +52,27 @@ const index = () => {
 
   return (
     <div className="App">
-    {step === 1 && (
-      <ImageDisplay
-        images={images}
-        onNextStep={handleNextStep}
-        setCurrentImage={setCurrentImage}
-        randomIndex={randomIndex}
-      />
-    )}
+      {step === 1 && (
+        <ImageDisplay
+          images={images}
+          onNextStep={handleNextStep}
+          setCurrentImage={setCurrentImage}
+          randomIndex={randomIndex}
+        />
+      )}
       {step === 2 && <TitleInput onSubmit={handleTitleSubmit} />}
       {step === 3 && <TitlesDisplay titles={titles} image={currentImage} onNextStep={handleNextStep} />}
       {step === 4 && <Voting titles={titles} image={currentImage} ownTitle={ownTitle} onVote={handleVote} />}
-      {step === 5 && <ResultsDisplay titles={titles} votes={votes} image={currentImage} />}
+      {step === 5 && (
+  <>
+    {console.log('Titles:', titles)}
+    {console.log('Votes:', votes)}
+    {console.log('Image:', currentImage)}
+    <ResultsDisplay titles={titles} votes={votes} image={currentImage} />
+  </>
+)}
     </div>
   );
 };
 
-export default index;
+export default Index;
